b794147bef1f6446703a367e8d228aff4d2f3232
Rick South <rick@natamus.com>
2023-12-12 00:13:10 +0100

    Save and Load Inventories source code for 1.19.2.

---
+++ Fabric/src/main/java/com/natamus/saveandloadinventories/ModFabric.java
@@ -0,0 +1,34 @@
+package com.natamus.saveandloadinventories;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.saveandloadinventories.cmds.CommandListinventories;
+import com.natamus.saveandloadinventories.cmds.CommandLoadinventory;
+import com.natamus.saveandloadinventories.cmds.CommandSaveinventory;
+import com.natamus.saveandloadinventories.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			CommandListinventories.register(dispatcher);
+			CommandLoadinventory.register(dispatcher);
+			CommandSaveinventory.register(dispatcher);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
