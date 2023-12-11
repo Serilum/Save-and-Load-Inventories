e320b450adf3637945bc5f0eb61309c2b683b8d2
Rick South <rick@natamus.com>
2023-12-12 00:13:44 +0100

    Save and Load Inventories source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/saveandloadinventories/neoforge/events/NeoForgeCommandRegisterEvent.java
@@ -0,0 +1,22 @@
+package com.natamus.saveandloadinventories.neoforge.events;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.natamus.saveandloadinventories.cmds.CommandListinventories;
+import com.natamus.saveandloadinventories.cmds.CommandLoadinventory;
+import com.natamus.saveandloadinventories.cmds.CommandSaveinventory;
+import net.minecraft.commands.CommandSourceStack;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeCommandRegisterEvent {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent e) {
+		CommandDispatcher<CommandSourceStack> dispatcher = e.getDispatcher();
+
+		CommandListinventories.register(dispatcher);
+		CommandLoadinventory.register(dispatcher);
+		CommandSaveinventory.register(dispatcher);
+	}
+}
