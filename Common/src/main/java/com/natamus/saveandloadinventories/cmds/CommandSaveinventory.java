267dbcfc373cd1efa40d91f95ea02a080b08022e
Rick South <rick@natamus.com>
2024-03-09 13:51:54 +0100

    (v3.3) Updated the mod to use the Collective's improved MessageFunctions instead of StringFunctions.

--- Common/src/main/java/com/natamus/saveandloadinventories/cmds/CommandSaveinventory.java
+++ Common/src/main/java/com/natamus/saveandloadinventories/cmds/CommandSaveinventory.java
@@ -4,14 +4,14 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.natamus.collective.functions.MessageFunctions;
 import com.natamus.collective.functions.PlayerFunctions;
 import com.natamus.collective.functions.StringFunctions;
 import com.natamus.saveandloadinventories.util.Util;
-
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.ChatFormatting;
 
 public class CommandSaveinventory {
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -36,29 +36,29 @@ public class CommandSaveinventory {
 			player = source.getPlayerOrException();
 		}
 		catch (CommandSyntaxException ex) {
-			StringFunctions.sendMessage(source, "This command can only be executed as a player in-game.", ChatFormatting.RED);
+			MessageFunctions.sendMessage(source, "This command can only be executed as a player in-game.", ChatFormatting.RED);
 			return 1;
 		}
 		
 		String inventoryname = StringArgumentType.getString(command, "inventory-name").toLowerCase();
 		if (inventoryname.trim() == "") {
-			StringFunctions.sendMessage(source, "The inventory name '" + inventoryname + "' is invalid.", ChatFormatting.RED);
+			MessageFunctions.sendMessage(source, "The inventory name '" + inventoryname + "' is invalid.", ChatFormatting.RED);
 			return 0;
 		}
 		
 		String gearstring = PlayerFunctions.getPlayerGearString(player);
 		if (StringFunctions.sequenceCount(gearstring, "\n") < 40) {
-			StringFunctions.sendMessage(source, "Something went wrong while generating the save file content for your inventory.", ChatFormatting.RED);
+			MessageFunctions.sendMessage(source, "Something went wrong while generating the save file content for your inventory.", ChatFormatting.RED);
 			return 0;					
 		}
 		
 		if (!Util.writeGearStringToFile(inventoryname, gearstring)) {
-			StringFunctions.sendMessage(source, "Something went wrong while saving the content of your inventory as '" + inventoryname + "'.", ChatFormatting.RED);
+			MessageFunctions.sendMessage(source, "Something went wrong while saving the content of your inventory as '" + inventoryname + "'.", ChatFormatting.RED);
 			return 0;							
 		}
 		
-		StringFunctions.sendMessage(source, "Successfully saved your inventory as '" + inventoryname + "'.", ChatFormatting.DARK_GREEN);
-		StringFunctions.sendMessage(source, "You can load it with the command '/loadinventory " + inventoryname + "'.", ChatFormatting.DARK_GREEN);
+		MessageFunctions.sendMessage(source, "Successfully saved your inventory as '" + inventoryname + "'.", ChatFormatting.DARK_GREEN);
+		MessageFunctions.sendMessage(source, "You can load it with the command '/loadinventory " + inventoryname + "'.", ChatFormatting.DARK_GREEN);
 		return 1;
 	}
 }
