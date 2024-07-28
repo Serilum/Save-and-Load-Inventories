0fb5dab9f7b55c89a285fc6ad96d0ac4579480c3
Rick South <rick@natamus.com>
2024-07-28 07:16:32 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/saveandloadinventories/ModFabric.java
+++ Fabric/src/main/java/com/natamus/saveandloadinventories/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.saveandloadinventories;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.saveandloadinventories.cmds.CommandListinventories;
 import com.natamus.saveandloadinventories.cmds.CommandLoadinventory;
 import com.natamus.saveandloadinventories.cmds.CommandSaveinventory;
@@ -12,6 +13,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
