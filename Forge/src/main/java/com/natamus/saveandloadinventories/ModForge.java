8f73f1bec37efebf54786603c27e633b1d960484
Rick South <rick@natamus.com>
2024-07-28 07:16:29 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/saveandloadinventories/ModForge.java
+++ Forge/src/main/java/com/natamus/saveandloadinventories/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.saveandloadinventories;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.saveandloadinventories.forge.events.ForgeCommandRegisterEvent;
 import com.natamus.saveandloadinventories.util.Reference;
 import net.minecraftforge.common.MinecraftForge;
@@ -13,6 +14,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
